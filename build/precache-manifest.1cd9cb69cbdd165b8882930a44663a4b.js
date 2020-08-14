75eec6a619287ab3613b67c92a8ea44d514d67d8
Aaron31 <aarongonzales531spam@gmail.com>
2020-08-13 17:51:38 -0700

    pushing to heroku for deployment

---
+++ build/precache-manifest.1cd9cb69cbdd165b8882930a44663a4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bc563189ff63ff73779ca588bfe3685",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebe81999184ffa6fdc37",
+    "url": "/static/css/main.f4b28a38.chunk.css"
+  },
+  {
+    "revision": "2d2570c74756282ea0f7",
+    "url": "/static/js/2.208aeb65.chunk.js"
+  },
+  {
+    "revision": "ebe81999184ffa6fdc37",
+    "url": "/static/js/main.f6c62231.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "897104c5d94674e65e2cbd02a7ee1242",
+    "url": "/static/media/oLogo.897104c5.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shoppingbag.30f07071.svg"
+  }
+]);
