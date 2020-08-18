0e02fd3244fd8522e33929c9d0981550870d3d46
Aaron31 <aarongonzales531spam@gmail.com>
2020-08-18 16:38:39 -0700

    mobile responsiveness for homepage

--- client/src/components/nav-two/nav-two.js
+++ client/src/components/nav-two/nav-two.js
@@ -9,6 +9,9 @@ const Nav = () => (
                 <span className="subtitle is-4 has-text-white has-backgorund-white">
                 FREE U.S SHIPPING ON ORDERS $60+! <span className=" shop pl-3">SHOP NOW</span>
                 </span>
+                <section className="subtitle has-text-danger">
+                Mobile responsiveness is still under construction! Current as of 8/20/20
+            </section>
             </Link> 
     </div>
 )
